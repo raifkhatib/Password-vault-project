538261e7132ea1e456a1e96b83febf7b3027f98c
Raif Khatib <raifrekfrtntrc@gmail.com>
2026-09-11 12:08:43 +0200

    Move homepage into HTML template

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return """
-    <h1>Password Vault</h1>
-    <p>The Flask application is running successfully.</p>
-    """
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
